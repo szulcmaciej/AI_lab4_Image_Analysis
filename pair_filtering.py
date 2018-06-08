c64942ab35ada9ffe242017d5c3bdbc56fa48f64
szulcmaciej <szulcmaciej1@gmail.com>
2018-06-08 14:05:35 +0200

    more refactors

--- pair_filtering.py
+++ pair_filtering.py
@@ -129,21 +129,3 @@ class RansacTransformModel:
     def __init__(self, pairs_for_transform, transformType):
         if transformType == 'affine':
             pass
-
-
-if __name__ == '__main__':
-    # file1 = 'images/nutella1.jpg'
-    # file2 = 'images/nutella2.jpg'
-
-    file1 = 'images/room1.png'
-    file2 = 'images/room2.png'
-
-    img1 = cv.imread(file1)
-    img2 = cv.imread(file2)
-
-    kp_pairs = get_filtered_keypoints_and_descriptions(img1, img2)
-
-    pairFilter = PairFilter(kp_pairs)
-    valid_pairs = pairFilter.filter_with_neighbourhood_integrity(6, 4)
-
-    show_pairs_on_images(file1, file2, valid_pairs)
